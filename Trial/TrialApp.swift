1c61b19a28bb95fd0064b8b9e575525702e1db66
Milos <milosmilosmilosmilosmilos@gmail.com>
2020-06-26 11:25:09 +0200

    Initial Commit

---
+++ Trial/TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialApp.swift
+//  Trial
+//
+//  Created by Milos on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
